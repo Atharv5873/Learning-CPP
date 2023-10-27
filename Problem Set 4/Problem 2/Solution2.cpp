fe39374f3b25d1482a2842d319eb82a9872aaed3
Atharv <atharv5873@gmail.com>
2023-10-27 17:09:05 +0530

    added solution 2

--- Problem Set 4/Problem 2/Solution2.cpp
+++ Problem Set 4/Problem 2/Solution2.cpp
@@ -1,1 +1,59 @@
-    
+#include<iostream>
+
+using namespace std;
+class Item {
+private:
+    int itemCode;
+    int itemPrice;
+
+public:
+    Item(int code, int price) : itemCode(code), itemPrice(price) {}
+
+    int getCode()
+    { 
+        return itemCode; 
+    }
+    int getPrice()
+    { 
+        return itemPrice; 
+    }
+};
+
+class Bill
+{
+    private:
+        Item item;
+        int quantity;
+    public:
+        Bill(Item item, int quantity) : item(item), quantity(quantity)
+        {
+            if(quantity<0)
+            {
+                throw invalid_argument("Invalid Qiantity");
+            }
+        }
+        int findBill()
+        {
+            return item.getPrice()*quantity;
+        }
+};    
+int main() {
+    Item i1(101, 1050);
+    Item i2(102, 450);
+
+    try 
+    {
+        Bill b1(i1, -3);
+        cout << "Bill 1: Invalid quantity" << endl;
+    } 
+    catch (const invalid_argument e) 
+    {
+        cerr << "Bill 1: " << e.what() << endl;
+    }
+
+    Bill b2(i2, 4);
+    cout << "Bill 2: " << b2.findBill() << endl;
+
+    return 0;
+}
+
